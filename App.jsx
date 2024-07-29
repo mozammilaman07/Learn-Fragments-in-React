578156863548488340dddd748088f87aa2540109
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-29 23:04:39 +0530

    using fragments

--- App.jsx
+++ App.jsx
@@ -1,18 +1,19 @@
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div>
-      <div>Healthy Food</div>
+    <React.Fragment>
+      <h1>Healthy Food</h1>
       <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+        <li class="list-group-item">Green Vegetable</li>
+        <li class="list-group-item">Chicken</li>
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Milk</li>
+        <li class="list-group-item">Egg</li>
       </ul>
-    </div>
+    </React.Fragment>
   );
 }
 
