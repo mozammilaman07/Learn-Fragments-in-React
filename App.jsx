2cc7a6c924e811ef2c007ee7c9a0e2a1f11d99dc
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-29 22:53:34 +0530

    add files

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <div>Healthy Food</div>
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
