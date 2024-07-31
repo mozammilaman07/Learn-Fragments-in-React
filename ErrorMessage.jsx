a5b0adc0a8b6f851d24d2c6a9c1453bcb4647bd7
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-31 20:04:31 +0530

    change in line 1

--- ErrorMessage.jsx
+++ ErrorMessage.jsx
@@ -1,12 +1,4 @@
-const ErrorMessage = () => {
-  let foodItems = [
-    "Green Vegetable",
-    "Chicken",
-    "Dal",
-    "Milk",
-    "Egg",
-    "Fruits",
-  ];
-  return <>{foodItems.length == 0 && <h2>Bucket is empty</h2>}</>;
+const ErrorMessage = ({ items }) => {
+  return <>{items.length == 0 && <h2>Bucket is empty</h2>}</>;
 };
 export default ErrorMessage;
