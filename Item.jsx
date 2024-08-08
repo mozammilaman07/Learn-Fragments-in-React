353a65f308d9e29f83a2a0869289cd3f4d4cd7d2
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-08 23:11:36 +0530

    added a button

--- Item.jsx
+++ Item.jsx
@@ -2,7 +2,10 @@ import styles from "./Item.module.css";
 const Item = ({ foodItems }) => {
   return (
     <>
-      <li className={`${styles["foodName"]} list-group-item`}>{foodItems}</li>
+      <li className={`${styles["foodName"]} list-group-item`}>
+        {foodItems}
+        <button className={`${styles.button} btn btn-info`}>Buy</button>
+      </li>
     </>
   );
 };
